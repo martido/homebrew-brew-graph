58fcc3db12abdd809326a2cc7ad8dd6988ec6f92
Martin Dobmeier <martin.dobmeier@gmail.com>
2014-11-21 22:07:45 +0100

    Fix URL

--- brew-graph.rb
+++ brew-graph.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class BrewGraph < Formula
   homepage "https://github.com/martido/brew-graph"
-  url "https://github.com/martido/homebrew-brew-graph/releases/download/v1.0/brew-graph-1.0.zip"
+  url "https://github.com/martido/brew-graph/releases/download/v1.0/brew-graph-1.0.zip"
   sha1 "0ee1cf73d0e11f5f81ec061ed1c1982842926240"
 
   def install
