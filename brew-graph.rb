794c9f2eb01f0215c8af98fa7c580562dc39dfa3
Martin Dobmeier <martin.dobmeier@gmail.com>
2017-11-26 14:30:10 +0100

    brew-graph 2.2

--- brew-graph.rb
+++ brew-graph.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class BrewGraph < Formula
   homepage "https://github.com/martido/brew-graph"
-  url "https://github.com/martido/brew-graph/releases/download/2.1/brew-graph-2.1.zip"
-  sha256 "b27ac760d2b0527dc5d1d25dfe2a9ae95e163208c9df18a69cca3ac06e87b0e1"
+  url "https://github.com/martido/brew-graph/releases/download/2.2/brew-graph-2.2.zip"
+  sha256 "379c5d5dbe40afe99d009b74636e300fc3f3af2b78aadace35891dab61e86a75"
 
   def install
     bin.install "brew-graph.rb"
