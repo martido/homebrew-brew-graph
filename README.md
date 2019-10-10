7988657805bfbcc4d84545b5ba8c73f20d450eb8
Martin Dobmeier <martin.dobmeier@gmail.com>
2019-10-10 22:47:20 +0200

    brew-graph 2.2.1

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+**Attention:** This tap repository is **deprecated**. [brew-graph][1] is now available as an external command. The command is equivalent to this version, but there's no need to install a formula. To switch to the new version:
+
+    brew untap martido/homebrew-brew-graph
+    brew uninstall brew-graph
+    brew tap martido/homebrew-graph
+
+**There will be no future updates of brew-graph via this tap repository.**
+
+# homebrew-brew-graph
+
 A Homebrew formula for the [brew-graph][1] script.
 
 Tap and install in one command with
@@ -11,4 +21,4 @@ Alternatively, in two steps
 
 The command will be available as `brew graph` and `brew-graph`.
 
-[1]: https://github.com/martido/brew-graph
+[1]: https://github.com/martido/homebrew-graph
--- brew-graph.rb
+++ brew-graph.rb
@@ -2,12 +2,31 @@ require "formula"
 
 class BrewGraph < Formula
   homepage "https://github.com/martido/brew-graph"
-  url "https://github.com/martido/brew-graph/releases/download/2.2/brew-graph-2.2.zip"
-  sha256 "379c5d5dbe40afe99d009b74636e300fc3f3af2b78aadace35891dab61e86a75"
+  url "https://github.com/martido/homebrew-graph/releases/download/2.2.1/brew-graph-2.2.1.zip"
+  sha256 "84becabfe46f59655e810214e777bffd82f0e7b41263c2c08bdc43527a881b04"
 
   def install
     bin.install "brew-graph.rb"
     mv "#{bin}/brew-graph.rb", "#{bin}/brew-graph"
+  end
+
+  def caveats
+    <<~EOS
+    
+      \e[#{31}m**************************** PLEASE NOTE *****************************\e[0m
+      brew-graph is now available as an external command.
+      The command is equivalent to this version,
+      but there's no need to install a formula.
+      
+      To switch to the new version:
+
+        brew untap martido/homebrew-brew-graph
+        brew uninstall brew-graph
+        brew tap martido/homebrew-graph
+
+      \e[#{31}mThere will be no future updates of brew-graph via this tap repository.\e[0m
+
+    EOS
   end
 
   test do
